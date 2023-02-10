4ad036a785bb249c20079f1630be02fadc267475
SandevD <sandev.net@gmail.com>
2023-02-10 13:00:27 +0530

    ✨Auth Login logout and register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../img/logo.png"
+import { AuthContext } from './../context/authContext';
 
 const Navbar = () => {
+
+  const { currentUser, logout } = useContext(AuthContext);
 
   return (
     <div className="navbar">
@@ -30,11 +34,14 @@ const Navbar = () => {
           <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <span>John</span>
-          <span>Logout</span>
-          {/* <Link className="link" to="/login">
-            Login
-          </Link> */}
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
           <span className="write">
             <Link className="link" to="/write">
               Write
