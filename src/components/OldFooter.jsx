11447dd37372b4c03d05e314bc765c5ab5e2456d
SandevD <sandev.net@gmail.com>
2023-02-25 14:05:18 +0530

    tailwind old footer and navbar

---
+++ src/components/OldFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from "../img/logo.png";
+
+const OldFooter = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made with ♥️ by <b>@sandev.net</b>.
+      </span>
+    </footer>
+  )
+}
+
+export default OldFooter
